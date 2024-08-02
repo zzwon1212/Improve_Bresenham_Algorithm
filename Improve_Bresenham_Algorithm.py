9eca5127d605b3d44e7e2d590e141078e0ce6f7e
Jiwon LEE <zzwon1212@snu.ac.kr>
2024-08-02 12:18:12 +0900

    [Refactor] Simplify code by using absolute function
    Signed-off-by: Jiwon LEE <zzwon1212@snu.ac.kr>

--- Improve_Bresenham_Algorithm.py
+++ Improve_Bresenham_Algorithm.py
@@ -8,64 +8,57 @@ def get_grid_cells(point):
   Get grid cells that the line between a point and the origin passes through.
 
   Parameters:
-  - point (list or tuple): A point represented as a list or tuple of coordinates (x, y).
-                           Both x and y are expected to be floats.
+  -----------
+  point: list or tuple
+    A point represented as a list or tuple of coordinates (x, y).
+    Both x and y are expected to be floats.
+
+  Returns:
+  -----------
+  grid_cells: list
+    A list or tuple which contains lists or tuples of grid cell.
   """
 
-  # Make empty list for grid cells that the line goes through
+  # Make empty list for grid cells that the line goes through.
   grid_cells = []
 
-  # Get x and y
-  x = point[0]
-  y = point[1]
+  # Get the absolute value of x and y.
+  # We will consider only the first quadrant
+  # and later, get the grid cells back to the original quadrant.
+  x = abs(point[0])
+  y = abs(point[1])
 
   # Calculate a slope of the line between p and origin.
   slope = y / (x + 1e-6)
 
-  # Get sign of X and Y.
-  x_sign = 1 if x >= 0 else -1
-  y_sign = 1 if y >= 0 else -1
-
   # Get the last grid value of X.
-  if x >= 0:
-    x_max = np.ceil(x).astype(np.int32)
-  else:
-    x_max = np.floor(x).astype(np.int32)
+  x_max = np.ceil(x).astype(np.int32)
 
   # y_min will be updated during iteration.
   y_min = 0
 
   # Iterate for X
-  for i in range(0 + x_sign, x_max + x_sign, x_sign):
+  for i in range(1, x_max + 1):
 
     # Get the current value at the current X.
     y_value = slope * i
 
     # Get the last grid value of Y at the current X.
-    if y >= 0:
-      y_max = np.ceil(y_value).astype(np.int32)
-    else:
-      y_max = np.floor(y_value).astype(np.int32)
+    y_max = np.ceil(y_value).astype(np.int32)
 
     # Iterate for Y
-    for j in range(y_min + y_sign, y_max + y_sign, y_sign):
+    for j in range(y_min + 1, y_max + 1):
 
-      # Break if the current grid value of Y is bigger than original Y.
-      if y >= 0:
-        if j >= (y + 1):
-          break
-      else:
-        if j < (y - 1):
-          break
+      # Break if the current grid value of Y is larger than original Y.
+      if j >= (y + 1):
+        break
 
-      # Append the grid cells
-      grid_cells.append([i, j])
+      # Get the grid cells back to the original quadrant
+      # and append them to the list.
+      grid_cells.append([i * np.sign(point[0]), j * np.sign(point[1])])
 
     # Get the first grid value of Y at the next X.
-    if y >= 0:
-      y_min = np.floor(y_value).astype(np.int32)
-    else:
-      y_min = np.ceil(y_value).astype(np.int32)
+    y_min = np.floor(y_value).astype(np.int32)
     
   return grid_cells
 
@@ -75,25 +68,25 @@ def draw_grid_cells(point, grid_cells):
   the grid cells that the line passes through.
 
   Parameters:
-  - point (list or tuple): A point represented as a list or tuple of coordinates (x, y).
-                           Both x and y are expected to be floats.
-  - grid_cells (list or tuple): A list or tuple which contains the grid cell list or tuple.
+  -----------
+  point: list or tuple
+    A point represented as a list or tuple of coordinates (x, y).
+    Both x and y are expected to be floats.
+
+  grid_cells: list
+    A list or tuple which contains lists or tuples of grid cell.
   """
 
   # Get x and y
   x = point[0]
   y = point[1]
 
-  # Get sign of X and Y.
-  x_sign = 1 if x >= 0 else -1
-  y_sign = 1 if y >= 0 else -1
-
   # Make a plot.
   fig, ax = plt.subplots(figsize=(7, 7))
 
   # Draw the grid cells
   for (i, j) in grid_cells:
-    rect = patches.Rectangle((i - 0.5*(x_sign + 1), j - 0.5*(y_sign + 1)), 1, 1, facecolor="gold")
+    rect = patches.Rectangle((i - 0.5*(np.sign(x) + 1), j - 0.5*(np.sign(y) + 1)), 1, 1, facecolor="gold")
     ax.add_patch(rect)
   
   # Draw the line
